0099081cf6d94327b028f1811a5e2d528e031801
Ravi Teja Nallamothu <nallamothuraviteja9@gmail.com>
2025-11-24 18:19:30 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -8,8 +8,8 @@ public class Student {
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
         System.out.println("=====");
-         System.out.println("WELCOME TO JENKINS");
-         System.out.println("WELCOME TO the world");
+         System.out.println("WELCOME TO the JENKINS");
+
         
     }
 }
